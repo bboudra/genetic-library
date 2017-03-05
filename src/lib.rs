831cd2523ec25f2b657c1cfe8c92a1ecbcf7997d
Benjamin Boudra <benboudra@gmail.com>
2017-03-05 11:38:56 -0700

    create new project

---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+/// This library will hold a series of highly Generic trait definitions and utility methods
+/// implemented on those traits to allow the rapid development of genetic utilities and libraries.
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+    }
+}
