1e0fa2c9452f8337d4fcf36598fe318a0e9625a2
Benjamin Boudra <benboudra@gmail.com>
2017-03-05 11:41:54 -0700

    created new genetic_collection and genetic_utilities files

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
-/// This library will hold a series of highly Generic trait definitions and utility methods
+/// This library will hold a series of highly generic trait definitions and utility methods
 /// implemented on those traits to allow the rapid development of genetic utilities and libraries.
+
+mod genetic_collection;
+
 
 #[cfg(test)]
 mod tests {
